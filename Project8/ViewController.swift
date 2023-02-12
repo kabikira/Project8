71fe9a20fe39a290ab3827493740af22e032d32d
kabikira <kabikirar2@protonmail.com>
2023-02-12 17:43:58 +0900

    add Day40Challenge

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     var activatedButtons = [UIButton]()
     var solutions = [String]()
-
+    
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
@@ -152,16 +152,16 @@ class ViewController: UIViewController {
                 letterButton.layer.borderColor = UIColor.lightGray.cgColor
             }
         }
-//        cluesLabel.backgroundColor = .red
-//        answersLabel.backgroundColor = .blue
-//        buttonsView.backgroundColor = .green
+        //        cluesLabel.backgroundColor = .red
+        //        answersLabel.backgroundColor = .blue
+        //        buttonsView.backgroundColor = .green
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadLevel()
         
-            }
+    }
     
     @objc func letterTapped(_ sender: UIButton) {
         guard let buttonTitle = sender.titleLabel?.text else { return }
@@ -173,18 +173,18 @@ class ViewController: UIViewController {
     
     @objc func submitTapped(_ sender: UIButton) {
         guard let answerText = currentAnswer.text else { return }
-
+        
         if let solutionPosition = solutions.firstIndex(of: answerText) {
             activatedButtons.removeAll()
-
+            
             var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
             splitAnswers?[solutionPosition] = answerText
             answersLabel.text = splitAnswers?.joined(separator: "\n")
-
+            
             currentAnswer.text = ""
             score += 1
             correctAnswersCount += 1
-
+            
             if correctAnswersCount == 7 {
                 let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
@@ -201,9 +201,9 @@ class ViewController: UIViewController {
     func levelUp(action: UIAlertAction) {
         level += 1
         solutions.removeAll(keepingCapacity: true)
-
+        
         loadLevel()
-
+        
         for btn in letterButtons {
             btn.isHidden = false
         }
@@ -211,11 +211,11 @@ class ViewController: UIViewController {
     
     @objc func clearTapped(_ sender: UIButton) {
         currentAnswer.text = ""
-
+        
         for btn in activatedButtons {
             btn.isHidden = false
         }
-
+        
         activatedButtons.removeAll()
     }
     
@@ -223,43 +223,47 @@ class ViewController: UIViewController {
         var clueString = ""
         var solutionString = ""
         var letterBits = [String]()
-
-        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
-            if let levelContents = try? String(contentsOf: levelFileURL) {
-                var lines = levelContents.components(separatedBy: "\n")
-                lines.shuffle()
-                
-
-                for (index, line) in lines.enumerated() {
-                    let parts = line.components(separatedBy: ": ")
-                    let answer = parts[0]
-                    let clue = parts[1]
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let levelFileURL = Bundle.main.url(forResource: "level\(self.level)", withExtension: "txt") {
+                if let levelContents = try? String(contentsOf: levelFileURL) {
+                    var lines = levelContents.components(separatedBy: "\n")
+                    lines.shuffle()
                     
-
-                    clueString += "\(index + 1). \(clue)\n"
-
-                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
-                    solutionString += "\(solutionWord.count) letters\n"
-                    solutions.append(solutionWord)
-
-                    let bits = answer.components(separatedBy: "|")
-                    letterBits += bits
+                    
+                    for (index, line) in lines.enumerated() {
+                        let parts = line.components(separatedBy: ": ")
+                        let answer = parts[0]
+                        let clue = parts[1]
+                        
+                        
+                        clueString += "\(index + 1). \(clue)\n"
+                        
+                        let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                        solutionString += "\(solutionWord.count) letters\n"
+                        self.solutions.append(solutionWord)
+                        
+                        let bits = answer.components(separatedBy: "|")
+                        letterBits += bits
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                self.cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+                self.answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                letterBits.shuffle()
+                
+                if letterBits.count == self.letterButtons.count {
+                    for i in 0 ..< self.letterButtons.count {
+                        
+                        self.letterButtons[i].setTitle(letterBits[i], for: .normal)
+                    }
                 }
             }
         }
-
-        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
-        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
-
-        letterBits.shuffle()
-
-        if letterBits.count == letterButtons.count {
-            for i in 0 ..< letterButtons.count {
-                
-                letterButtons[i].setTitle(letterBits[i], for: .normal)
-            }
-        }
-    }
         
+    }
+    
 }
 
